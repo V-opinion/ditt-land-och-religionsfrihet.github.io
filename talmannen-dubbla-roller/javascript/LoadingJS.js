aff4d9bb22086d3a239ccff20533e79a708f9c3a
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-02-26 23:15:01 +0100

    Update LoadingJS.js

--- talmannen-dubbla-roller/javascript/LoadingJS.js
+++ talmannen-dubbla-roller/javascript/LoadingJS.js
@@ -909,9 +909,9 @@ function GuideLoadingJS() {
     var url = window.location.href;
     var shotUrl;
     if (url.indexOf('mobile') > -1) {
-      shotUrl = removeStartFrom(url, '/mobile/');
-      shotUrl = removeStartFrom(shotUrl, '/articles/');
-      shotUrl += '/files/shot.jpg';
+      shotUrl = removeStartFrom(url, 'https://ditt-land-och-religionsfrihet.multireligionvalsystem.eu.org/talmannen-dubbla-roller/mobile/');
+      shotUrl = removeStartFrom(shotUrl, 'https://ditt-land-och-religionsfrihet.multireligionvalsystem.eu.org/talmannen-dubbla-roller//articles/');
+      shotUrl += 'https://ditt-land-och-religionsfrihet.multireligionvalsystem.eu.org/talmannen-dubbla-roller//files/shot.jpg';
     } else {
       if (pathname.indexOf('index.html') > -1) {
         pathname = pathname.replace('index.html', '');
@@ -1511,7 +1511,7 @@ function GuideLoadingJS() {
     createContactItem(item) {
       var $item = `<div class="${this.POP}Item" key="${item.type}">
           <div>
-            <img src="style/icon/contactWay_${item.type}.png" />
+            <img src="https://ditt-land-och-religionsfrihet.multireligionvalsystem.eu.org/talmannen-dubbla-roller/style/icon/contactWay_${item.type}.png" />
           </div>
           <span>${item.title}</span>
         </div>`;
