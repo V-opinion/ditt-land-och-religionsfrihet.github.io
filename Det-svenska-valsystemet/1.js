511792d72135bbb77c58a4fc3c024c17ced3e1bb
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-03-25 02:16:41 +0100

    Add files via upload

---
+++ Det-svenska-valsystemet/1.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/422.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/423.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/424.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/425.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/426.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/427.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/428.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/429.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/430.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/431.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/432.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/433.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/434.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/435.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/436.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/437.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/438.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/439.jpg"
+}
+];
---
+++ Det-svenska-valsystemet/2.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/422.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/442.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/424.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/443.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/426.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/444.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/428.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/445.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/430.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/446.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/432.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/447.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/434.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/448.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/436.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/449.jpg"
+},
+{
+ image: "Det-svenska-valsystemet_htm_files/438.jpg",
+ thumbnail: "Det-svenska-valsystemet_htm_files/450.jpg"
+}
+];
